0bade09b5c7ae992325c703673de98734018c969
Travis Walentin <travis.walentin@gmail.com>
2017-01-26 15:30:25 -0800

    added some story and made intro page pretty

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,4 @@
 angular.module('App')
-.controller('home', ['$scope', '$state', function ($scope, $state){
-    // $scope.changeState = function(){
-    //     $state.go('specs', {message: 'Here!'});
-    // };
-}])
 .controller('form', ['$scope', '$state', function ($scope, $state){
     // $scope.changeState = function(){
     //     $state.go('specs', {message: 'Here!'});
